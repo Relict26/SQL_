3a7fb9b69c1ff10fa57045f573def710bc4e4c03
Relict <vostryakovkirill@gmail.com>
2025-04-28 22:34:02 +0400

    Update042

--- src/test/java/data/SQLHelper.java
+++ src/test/java/data/SQLHelper.java
@@ -10,33 +10,41 @@ import java.sql.SQLException;
 
 public class SQLHelper {
     private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+    private static final int CONNECTION_TIMEOUT = 5; // seconds
 
     private SQLHelper() {
     }
 
     private static Connection getConn() throws SQLException {
-        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
+        String url = System.getProperty("db.url", "jdbc:mysql://localhost:3306/app");
+        String user = System.getProperty("db.user", "app");
+        String password = System.getProperty("db.password", "pass");
+
+        return DriverManager.getConnection(url, user, password);
     }
 
     @SneakyThrows
     public static DataHelper.VerificationCode getVerificationCode() {
         var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
-        var conn = getConn();
-        var code = QUERY_RUNNER.query(conn, codeSQL, new ScalarHandler<String>());
-        return new DataHelper.VerificationCode(code);
+        try (var conn = getConn()) {
+            var code = QUERY_RUNNER.query(conn, codeSQL, new ScalarHandler<String>());
+            return new DataHelper.VerificationCode(code);
+        }
     }
 
     @SneakyThrows
     public static void cleanDatabase() {
-        var connection = getConn();
-        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
-        QUERY_RUNNER.execute(connection, "DELETE FROM cards");
-        QUERY_RUNNER.execute(connection, "DELETE FROM users");
+        try (var connection = getConn()) {
+            QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+            QUERY_RUNNER.execute(connection, "DELETE FROM cards");
+            QUERY_RUNNER.execute(connection, "DELETE FROM users");
+        }
     }
 
     @SneakyThrows
     public static void cleanAuthCodes() {
-        var connection = getConn();
-        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+        try (var connection = getConn()) {
+            QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+        }
     }
 }
